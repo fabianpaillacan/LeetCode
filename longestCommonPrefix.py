85e4c0ec6755ae0f91c30b6ca48ff9bd87732649
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-05-13 12:35:49 -0400

    no funciona

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,23 +1,25 @@
-strs = ["flower","flow","flight"]
+#no me esta funcionando el test, con un arreglo de flower, flower, flower
+#solo me imprime fff, y no flower
+
+strs = ["flower", "flow", "flight"]
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-   
-        if not strs:  # Si la lista está vacía
-            print("No hay palabras para comparar")
-            output = ""
-        else:
-            firstWord = strs[0]
-            output = ""
-            
-            for letra in firstWord:
-                isAll = True
-                for palabra in strs[1:]:
-                    if letra not in palabra:
-                        isAll = False
-                        break
-                if isAll:
-                    output += letra
-                else:
-                    break  
+        if not strs:  
+            return ""
+
+        if all(s == strs[0] for s in strs):
+            if strs[0]: 
+                return strs[0][0] * len(strs)
+            else:
+                return ""
+
+        # Caso general: encontrar el prefijo común
+        output = ""
+        for i in range(len(strs[0])):
+            current_char = strs[0][i]
+            for word in strs[1:]:
+                if i >= len(word) or word[i] != current_char:
+                    return output
+            output += current_char
         return output
