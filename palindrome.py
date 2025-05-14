23920a9154063e04fa98843c61b4e5163fc3fdca
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-05-13 21:05:20 -0400

    optimizado

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,9 @@
 # LeetCode Problem 9: Palindrome Number
 # Problem: https://leetcode.com/problems/palindrome-number/
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        original = x
-        reversed_num = 0
-        
-        while x > 0:
-            reversed_num = reversed_num * 10 + (x % 10)
-            x = x // 10     
-        return original == reversed_num
+   def isPalindrome(self, x: int) -> bool:
+    cadena = str(x)
+    if cadena == ''.join(reversed(cadena)):
+        print("Es un palíndromo")
+    else:
+        print("No es un palíndromo")
