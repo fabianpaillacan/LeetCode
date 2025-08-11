5f7fbfba57dfaf7f4ad42ac5963117b3999aae85
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-08-11 12:16:06 -0400

    Agregar implementación de la solución para el problema de dividir dos enteros en Python

---
+++ medium/python/29. Divide Two Integers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        result = int(dividend / divisor)
+        if result < -2**31:
+            return -2**31
+        if result > 2**31 - 1:
+            return 2**31 - 1
+        return result
